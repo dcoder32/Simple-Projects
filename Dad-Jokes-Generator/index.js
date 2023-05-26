a2860d6d39c3bcf3ca61283456bfbede701e0d64
Derick <wunsps@gmail.com>
2023-05-26 18:53:33 +0200

    Proyect done, and working

---
+++ Dad-Jokes-Generator/index.js
@@ -0,0 +1,28 @@
+'use strict'
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+
+const apiKey = "+VQWuXQ95OwZicRdhp6Ekg==01kYs6tvOxqHbH4a";
+const options = {
+    method: 'GET',
+    headers: {
+        "X-Api-Key": apiKey,
+    },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getJoke() {
+    jokeEl.innerText = "Updating...";
+    btnEl.disable = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+
+    btnEl.disable = false;
+    btnEl.innerText = "Tell me a joke";
+
+    jokeEl.innerText = data[0].joke;
+}
+
+btnEl.addEventListener("click", getJoke);
